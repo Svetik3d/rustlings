3300c849618f97c0c685827ffe9cb8fdff2f69b7
sveta <sveta-a-popova@yandex.ru>
2023-08-27 16:29:58 +0300

    66/96

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,12 +14,12 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
 struct Team {
+    name: String,
     goals_scored: u8,
     goals_conceded: u8,
 }
@@ -39,6 +39,11 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        let team1 = Team {name: team_1_name.clone(), goals_scored: team_1_score, goals_conceded: team_2_score};
+        let team2 = Team {name: team_2_name.clone(), goals_scored: team_2_score, goals_conceded: team_1_score};
+        
+        scores.entry(team_1_name).and_modify(|team| {team.goals_scored += team_1_score; team.goals_conceded += team_2_score}).or_insert(team1);
+        scores.entry(team_2_name).and_modify(|team| {team.goals_scored += team_2_score; team.goals_conceded += team_1_score}).or_insert(team2);
     }
     scores
 }
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,7 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
 
 pub enum Command {
     Uppercase,
@@ -31,12 +30,19 @@ pub enum Command {
 mod my_module {
     use super::Command;
 
-    // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output = vec![];
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            let s = match command {
+                Command::Uppercase => {string.to_uppercase()},
+                Command::Trim => {string.trim().to_string()},
+                Command::Append(num) => {
+                    let mut string = string.clone();
+                    for _i in 1..=*num {string.push_str("bar");}
+                    string.to_string()
+                },
+            };
+            output.push(s)
         }
         output
     }
@@ -44,8 +50,7 @@ mod my_module {
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use crate::my_module::transformer;
     use super::Command;
 
     #[test]
